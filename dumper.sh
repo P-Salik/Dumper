3ceb02a67fa848ce3e2e38a113877a00db00f508
P-Salik <peersalik16@gmail.com>
2023-09-24 16:37:10 +0530

    Dumper: Use DumprX [skip ci]
    * Also update some vars
    
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- dumper.sh
+++ dumper.sh
@@ -5,23 +5,19 @@ mkdir -p /tmp/dump
 cd /tmp/dump
 export TZ=Asia/Kolkata
 export DEBIAN_FRONTEND=noninteractive
-apt-get install git curl python python3 wget aria2 -y
-git clone https://github.com/P-Salik/phoenix_firmware_dumper
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
-python2 get-pip.py
-pip install --upgrade pip
-pip install setuptools wheel docopt pycryptodome zip
-./phoenix_firmware_dumper/setup.sh
+apt install git curl python2 python3 python-pip wget -y
+git clone https://github.com/P-Salik/DumprX dumper
+./dumper/setup.sh
 
-echo $GH_TOKEN >> phoenix_firmware_dumper/.github_token 2>/dev/null
-echo $GH_ORG >> phoenix_firmware_dumper/.github_orgname 2>/dev/null
-echo $TG_TOKEN >> phoenix_firmware_dumper/.tg_token 2>/dev/null
-echo $TG_CHAT >> phoenix_firmware_dumper/.tg_chat 2>/dev/null
+echo $GH_TOKEN >> dumper/.github_token 2>/dev/null
+echo "P-Salik-Dumps" >> dumper/.github_orgname 2>/dev/null
+echo $TG_TOKEN >> dumper/.tg_token 2>/dev/null
+echo "-1001641837458" >> dumper/.tg_chat 2>/dev/null
 
 git config --global user.name $GH_USERNAME
 git config --global user.email $GH_USEREMAIL
 
 # Dump
-cd /tmp/dump/phoenix_firmware_dumper
+cd /tmp/dump/dumper
 ./dumper.sh $FIRMWARE_LINK
 echo Done!
